ed1a310000997e484c989e5a96a9a9841a6f28f4
Mohamed El Shenawy <mohamed211111@gmail.com>
2017-05-11 14:46:27 +0200

    Added Matrix Multiplication

--- Math Background Warm Up/linearA.m
+++ Math Background Warm Up/linearA.m
@@ -2,4 +2,10 @@
 A = [4 -5 ; -2 3]; 
 B = [13 ; -9]; 
 
+% Matrix Multiplication 'Dot product'
 C = A*B 
+
+% Matrix Element wise Multiplication 'Element-by-element multiplication' 
+
+C = A .* B 
+
