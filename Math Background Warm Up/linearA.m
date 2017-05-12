53272b546e8f4e20a191fb6753f51c26c00fce52
Mohamed Elshenawy <mohamed211111@gmail.com>
2017-05-12 15:56:12 +0200

    Added Octave linear Algebra

--- Math Background Warm Up/linearA.m
+++ Math Background Warm Up/linearA.m
@@ -6,6 +6,28 @@ B = [13 ; -9];
 C = A*B 
 
 % Matrix Element wise Multiplication 'Element-by-element multiplication' 
-
 C = A .* B 
 
+% Matrix Addition 
+A1 = [1 2 ; 3 4];
+B1 = [5 6 ; 7 8];
+C1 = A1+B1 
+
+% Matrix Subtraction 
+C1 = A1-B1
+
+% Scaler multiplication 
+S = 5 
+C1 =  S * A1 
+
+% Identiy Matrix 
+I = eye(3)
+
+% Matrix Transpose 
+T = A1' % or A1.'
+
+% Matrix Inverse 
+invA = pinv(A) %Pseudo inverse  
+
+% Multiplying Original matrix A by it's inverse by A will give the original Matrix A
+A * invA * A  
